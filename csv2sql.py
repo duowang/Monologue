d1e11ca03fdc1689aa33bd410d97292319f08b35
Duo Wang <duowang84@gmail.com>
2017-07-11 21:54:49 +0000

    update table schema

--- csv2sql.py
+++ csv2sql.py
@@ -3,12 +3,14 @@ import psycopg2
 import os
 
 
-def csv2sql(filename):
-    conn = psycopg2.connect("dbname='monologue' user='dwang' host='localhost'")
+def csv2sql(dirname, filename):
+    user = ""
+    connect_str = "dbname={user} user={user} password={user} host='localhost'"
+    conn = psycopg2.connect(connect_str.format(user=user))
     cur = conn.cursor()
-    with open("newsmax/" + filename, 'r') as csvfile:
+    with open(dirname + "/" + filename , 'r') as csvfile:
         # sql = "insert into monologue_test values ('{author}', '{date}', '{source}', '{content}'"
-        sql = "insert into monologue_test values ($${author}$$, $${date}$$, $${source}$$, $${content}$$)"
+        sql = "insert into monologue (author, date, source, content) values ($${author}$$, $${date}$$, $${source}$$, $${content}$$)"
         date = filename[:-4]
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -19,16 +21,21 @@ def csv2sql(filename):
             try:
                 cur.execute(insert_sql)
             except psycopg2.IntegrityError as e:
-                print "existed"
+                print dirname, filename, "existed"
                 print row['name'], row['monologue']
                 conn.commit()
                 continue
+            except Exception:
+                print dirname, filename
+                exit
             conn.commit()
     conn.close()
 
 
 if __name__ == '__main__':
-    for filename in os.listdir("newsmax/"):
-        if filename.startswith("2015") or filename.startswith("2016"):
-            print filename
-            csv2sql(filename)
+    #csv2sql("newsmax", "2017-07-10.csv")
+    #exit()
+    for dirname in ["/2009", "/2010", "/2011", "/2012", "/2013", "/2014", "/2015", "/2016", ""]:
+        for filename in os.listdir("newsmax" + dirname):
+            if filename[-4:] == ".csv":
+                csv2sql("newsmax" + dirname, filename)
--- schema.sql
+++ schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE monologue_test (
     content     text,
     CONSTRAINT content UNIQUE(content)
 );
+
+CREATE TABLE monologue(
+    id          serial primary key,
+    author      varchar(20),
+    date        date,
+    source      varchar(20),
+    content     text,
+    CONSTRAINT content UNIQUE(content)
+);
